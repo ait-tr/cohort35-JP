d9ba26fc43dd9f10436015650a40f15ccee0a11f
StanislavPB <perminovsb@gmail.com>
2024-03-26 17:27:16 +0000

    Lesson 02

---
+++ back_end/lesson_02/code/interrupt/InterruptExample.java
@@ -0,0 +1,32 @@
+package lesson_02.code.interrupt;
+
+import lesson_01.code.part1.ThreadExample1;
+
+public class InterruptExample {
+    public static void main(String[] args) {
+        Thread tr = new Thread(
+                () -> {
+                    boolean flag = true;
+                    while (flag) {
+                        System.out.println("Working ...");
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e){
+                            System.out.println("Exception!");
+                            flag = false;
+                        }
+                    }
+                }
+        );
+
+        tr.start();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        tr.interrupt();
+    }
+}
