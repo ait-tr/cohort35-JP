e339b1ee927b32bd0735b4dcf6794d549c29686c
StanislavPB <perminovsb@gmail.com>
2024-03-28 18:36:40 +0000

    Lesson 04

--- back_end/lesson_04/code/reflection/example4/SimpleExercise.java
+++ back_end/lesson_04/code/reflection/example4/SimpleExercise.java
@@ -1,5 +1,10 @@
 package lesson_04.code.reflection.example4;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class SimpleExercise {
 
     /*
@@ -10,5 +15,46 @@ public class SimpleExercise {
     5) создать объект из поля str и заменить его на "группа 35"
     6) создать объекты из метода1 и метода3 и запустить их
      */
+
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException {
+
+        Simple simple = new Simple();
+
+        // создаем новый объект Класс из нашего объекта simple
+        Class cls = simple.getClass();
+        System.out.println("имя класса: " + cls.getName());
+
+        // получаем имя конструктора
+        Constructor constructor = cls.getConstructor();
+        System.out.println("Имя конструктора: " + constructor.getName());
+
+        // получаем публичные методы
+        Method[] methods = cls.getMethods();
+
+        for (Method method : methods) {
+            System.out.println(method);
+        }
+
+        // создаем объект нужного метода и параметра класса
+        Method methodCall2 = cls.getDeclaredMethod("method2",int.class);
+        methodCall2.invoke(simple,25);
+
+
+        // создаем объект поля с помощью имени поля
+
+        Field field = cls.getDeclaredField("str");
+        field.setAccessible(true);
+        field.set(simple,"группа 35");
+
+        Method methodCall1 = cls.getDeclaredMethod("method1");
+        methodCall1.invoke(simple);
+
+
+        Method methodCall3 = cls.getDeclaredMethod("method3");
+        methodCall3.setAccessible(true);
+        methodCall3.invoke(simple);
+
+    }
+
 
 }
